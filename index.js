df1aad1c7190d936f55076565167a01ba7508f72
Francisco <88842468+franciscofrask@users.noreply.github.com>
2023-01-07 13:52:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ const corsOptions = {
 
 app.use(cors(origin: "*"));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
