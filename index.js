ab722d090ac0c235fdd185fc106ce872a7c736d7
Francisco <88842468+franciscofrask@users.noreply.github.com>
2023-01-07 13:59:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,19 +14,7 @@ dotenv.config();
 conectarDB();
 
 // Configurar CORS
-const whitelist = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      // Puede consultar la API
-      callback(null, true);
-    } else {
-      // No esta permitido
-      callback(new Error("Error de Cors"));
-    }
-  },
-};
 
 app.use(cors(origin: "*"));
 
