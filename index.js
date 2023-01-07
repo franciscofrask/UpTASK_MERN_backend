c60363c78a9ed37ef24da20b2fb9e3cba1dc7e7e
Francisco <88842468+franciscofrask@users.noreply.github.com>
2023-01-07 13:45:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const corsOptions = {
   },
 };
 
-app.use(cors(corsOptions));
+app.use(cors(origin: "*"));
 
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
@@ -46,7 +46,7 @@ import { Server } from "socket.io";
 const io = new Server(servidor, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
   },
 });
 
